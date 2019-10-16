efb3ec38096247fc7a0201214436bce43c2bb435
tutt <nhout140597@gmail.com>
2019-10-16 10:56:43 +0700

    fix: upload and delete file

--- view/list.php
+++ view/list.php
@@ -27,33 +27,45 @@
 				$arr_upload[] =  $value;
 				$local_file = dirname(__DIR__).'/'.'page'. $value .'.html';
 				$ftp_path = '/'.'page'. $value .'.html';
-				//$upload = ftp_put($conn_id, $ftp_path, $local_file, FTP_ASCII);
 
-				$regex = '/src="([^"]+)"/'; //get string in scr= ""
+				//get string in scr= ""
+				$regex = '/src="([^"]+)"/'; 
+				//return all result link folder image.
 				preg_match_all($regex, $content, $matches, PREG_SET_ORDER, 0);
+				//folder ftp page.
 				$dir_page = ftp_pwd($conn_id).$value;
-
+				//return all folder in folder root ftp
 				$content_ftp = ftp_nlist($conn_id,ftp_pwd($conn_id));
-				
+				//Check if the public folder is in the ftp directory .
 				if(in_array($dir_page,$content_ftp)){
+
 					foreach($matches as $val){
-						$get_link_img = "C:/xampp/htdocs/page-master".$val[1];
+						//get path of images  in the local directory.
+						$get_link_img = LOCAL_FILE.$val[1];
+						//pathinfo: get infomation path tranmission.
 						$path_info = pathinfo($get_link_img);
+						//return name image.
 						$get_img_name = $path_info['basename'];
+						//path folder contain images 
 						$link_path = $dir_page.'/'.$get_img_name;
-						if(ftp_put($conn_id,$link_path,$get_link_img,FTP_ASCII)){
+						//using ftp_put transfer images  to folder ftp contain page 
+						if(ftp_put($conn_id,$link_path,$get_link_img,FTP_BINARY)){
 							$img_using[] = $get_img_name;
 						}
 					}
+					//check the content in folder ftp 
 					$check_content_child_folder = ftp_nlist($conn_id, $dir_page);
 
 					foreach($check_content_child_folder as $check){
+						var_dump($check_content_child_folder);die();
 						$path_info_check = pathinfo($check);
 						if($path_info_check['extension']!='hmtl'){
 							$list_content[] = $path_info_check['basename'];
+							//var_dump($list_content);die();
 						}
 					}
 
+					//check images in folder page ftp have empty 
 					if (!empty($img_using)) {
 						$get_array_non_using = array_diff($list_content, $img_using);
 						foreach($get_array_non_using as $remove) {
@@ -63,18 +75,13 @@
 						}
 					}
 				}else{
+					//If the page directory is not already in the ftp 
 					if (ftp_mkdir($conn_id,$dir_page)) {
 						foreach($matches as $val){
-							// /ckeditor/kcfinder/upload/images/doc.png
-
-							$get_link_img = "C:/xampp/htdocs/page-master".$val[1];
-							// $get_link_img = "C:/xampp/htdocs/page-master/ckeditor/kcfinder/upload/images/doc.png";
+							$get_link_img = LOCAL_FILE.$val[1];
 							$path_info = pathinfo($get_link_img);
-
 							$get_img_name = $path_info['basename'];
-							// $link_path = $dir_page.'/doc.png';
-						    $link_path = $dir_page.'/'.$get_img_name;
-
+							$link_path = $dir_page.'/'.$get_img_name;
 							if(!ftp_put($conn_id,$link_path,$get_link_img,FTP_BINARY)){
 								echo "Error";
 							}
@@ -82,26 +89,18 @@
 					}
 				}
 				$link_temp = "file_temp.html";
-				$partern = "/\ckeditor\/kcfinder/\upload\/images/\/m";
-				$replace_img = $link_path;
-				$subject_img = file_get_contents($local_file);
-				$result = file_put_contents($link_temp,preg_replace($pattern,$replace_img,$subject_img));
-				// die();
+				$ftp_file= "/".$value."/";
+				$replace_up = str_replace("ckeditor/kcfinder/upload/images/",$ftp_file,file_get_contents($local_file));
+				file_put_contents($link_temp,$replace_up);
 				$upload = ftp_put($conn_id, $dir_page . "/page$value.html", $link_temp, FTP_ASCII);
-
 				file_put_contents($link_temp, "");
 				$file_update = 'page'. $value .'.html';
 				$dataOperation = new DataOperation();
 				// Update link upload 
 				$dataOperation->updateFile($value,$ftp_path);
-
 				// Update status page
 				 $dataOperation->updateStatusPage($value);
-			    // die();
 			}
-			// var_dump($dir_page);die();
-			
-
 			if ($upload) {
 				$id_upload = implode('-', $arr_upload);
 				echo "<div class='container alert alert-success'>Upload successfull $id_upload.</div>";
@@ -114,18 +113,35 @@
 	//Delete page
 	if (isset($_POST['submitDel'])) {
 			foreach ($_POST['checkbox'] as $key => $value) {
-				//Delete one page
-				$link  = dirname(__DIR__).'/'.'page'. $value .'.html';//Address page on local 
-				$ftp_path = '/'.'page'. $value .'.html';
-
-				$file = 'page'. $value .'.html';//name page on FTP server
-				$contents_on_server = ftp_nlist($conn_id, '.');//return list page in folder on FTP server
-				
-				if (in_array($file, $contents_on_server) && ($file != NULL)) {
+				//Address page on local 
+				$link  = dirname(__DIR__).'/'.'page'. $value .'.html';
+				//return list page in folder on FTP server
+				$contents_on_server = ftp_nlist($conn_id,'.');
+				//name folder page
+				$folder_page_ftp = $value;
+				//check folder page already in ftp .
+				if (in_array($folder_page_ftp, $contents_on_server)) {
 					$arDel[] = $value;
+					//delete page in database
 					$page = $obj->deletePage($value);
+					//delete page in local 
 					unlink($link);
-					$del_file = ftp_delete($conn_id, $ftp_path);
+					//check content inside in folder page.
+					$content_folder = ftp_nlist($conn_id, $folder_page_ftp . "/");
+					foreach ($content_folder as $need_remove) {
+						$need_remove ="/".$need_remove;
+						//Delete content in folder page ftp
+						if (!ftp_delete($conn_id, $need_remove)) {
+							echo "Cant delete content inside folder $value.";
+							break;
+						}
+					}
+					$name_folder_page = "/". $folder_page_ftp;
+					//delete folder page.
+					if (!ftp_rmdir($conn_id, $name_folder_page)) {
+						echo "Cant remove this folder $value at it's address: $folder_page_ftp";
+						break;
+					}
 				}else{
 					$arDel[] = $value;
 					$page = $obj->deletePage($value);
@@ -168,7 +184,7 @@
 								<?php 
 									if($row['status'] == "Public" ){
 									?>
-										<a href="cmsftppage.vn<?php echo $row['upload']; ?>" target="_blank" ><?php echo $row['title'];?></a>
+										<a href="ftp://169.254.214.253<?php echo '/'.$row['id'].$row['upload']; ?>" target="_blank" ><?php echo $row['title'];?></a>
 									<?php
 									}else{
 										 echo $row['title'];
