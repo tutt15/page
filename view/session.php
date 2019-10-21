840910f3e6fa702380cb4156932b4c6dabac25ea
tutt <nhout140597@gmail.com>
2019-10-21 13:57:37 +0700

    edit file session

--- view/session.php
+++ view/session.php
@@ -3,11 +3,8 @@
 
 	$username=$_SESSION['username'];
 	$conn = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
-	$sql=mysqli_query($conn,"select username from user where username='$username' ");
-
-	$row=mysqli_fetch_array($sql,MYSQLI_ASSOC);
-
-	$login=$row['username'];
+	$sql_login = new DataOperation();
+	$sql = $sql_login ->listPageById("user", ["username" => $username],["username"]);
 
 	if(!isset($_SESSION['username'])){
 	  header("location:login.php");
